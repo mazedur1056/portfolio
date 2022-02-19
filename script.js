49059f0d80bfee381299a042881e3a0c791b4b07
mazedur1056 <mazedur15-1056@diu.edu.bd>
2022-02-19 14:09:11 +0600

    commited

---
+++ script.js
@@ -0,0 +1,14 @@
+// const navbar = document.querySelector('#navbar')
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', function() {
+  const lastPosition = window.scrollY
+  if (lastPosition > 20 ) {
+    navbar.classList.add('nav-bg')
+  } 
+//   else if (navbar.classList.contains('nav-bg')) {
+//     navbar.classList.remove('nav-bg')
+//   } 
+  else {
+    navbar.classList.remove('nav-bg')
+  }
+})
