b787a502bf9221929dbed2656b22cf8e8f631a0b
mazedur1056 <mazedur15-1056@diu.edu.bd>
2022-04-06 02:12:32 +0600

    commited

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ..............add background to navbar......................
 // const navbar = document.querySelector('#navbar')
 const navbar = document.getElementById('navbar');
 window.addEventListener('scroll', function() {
@@ -12,3 +13,11 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('nav-bg')
   }
 })
+
+// ............typing Animation..................
+var typed = new Typed(".typing", {
+  strings: ["a learner", "an AI developer", "a data analyst", "a web designer", "a photographer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
